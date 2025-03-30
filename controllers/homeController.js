2b56a6140cb8c520b2dd36132a22920d660ed34e
Kyle W <125979751+MisterShika@users.noreply.github.com>
2025-03-30 17:43:28 +0900

    Folders now displaying on index

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,17 +1,34 @@
 const db = require("../db/queries");
+const fs = require("fs").promises;
 
-async function getHome (req, res) {
+async function getHome(req, res) {
     const userId = req.user?.id || null;
-    let folder;
-    if(userId){
-        folder = await db.getFoldersByUserId(userId) || null;
+    let folders = [];
+    let files;
+
+    if (userId) {
+        const folderPath = `uploads/${userId}/`;
+        try {
+            const files = await fs.readdir(folderPath, { withFileTypes: true });
+            folders = files
+                .filter(file => file.isDirectory())
+                .map(dir => ({
+                    name: dir.name,
+                    path: `uploads/${userId}/${dir.name}`
+                }));
+        } catch (err) {
+            console.error("Error reading directory:", err);
+            return res.status(500).send("Error reading directory");
+        }
+        files = await db.getFoldersByUserId(userId) || null;
     }
 
-    res.render("index",{
+    res.render("index", {
         userId,
         email: req.user?.email || null,
         authenticated: req.isAuthenticated(),
-        folder
+        files,
+        folders
     });
 }
 
